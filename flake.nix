cab6979eae0d5761b316baa2ee464b87f86fced8
mrkirby153 <mr.austinwhyte@gmail.com>
2023-04-30 12:19:59 -0700

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,16 +3,36 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = 
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        src = self;
-        name = "dwmblocks";
-        buildInputs = with nixpkgs; [ xorg.libX11 pkg-config ];
-        makeFlags = [ "PREFIX=$(out)" ];
-      };
-  };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        packages = rec {
+          dwmblocks = pkgs.stdenv.mkDerivation {
+            src = self;
+            name = "dwmblocks";
+            buildInputs = with pkgs; [ xorg.libX11 pkg-config ];
+            makeFlags = [ "PREFIX=$(out)" ];
+          };
+          default = dwmblocks;
+        };
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            xorg.libX11
+            pkg-config
+          ];
+        };
+        apps = rec {
+          dwmblocks = utils.lib.mkApp {
+            drv = packages.dwmblocks;
+          };
+          default = dwmblocks;
+        };
+      }
+    );
 }
